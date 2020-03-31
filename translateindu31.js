8605f74f273bfe35f1c86a70e7179b10de879e31
Indunil1992 <indunil@adroitlogic.com>
2020-03-31 12:05:51 +0530

    1

--- translateindu31.js
+++ translateindu31.js
@@ -3,48 +3,17 @@ const translate = new AWS.Translate();
 
 exports.handler = async (event) => {
     try {
-        let data = await translate.translateText({
-            SourceLanguageCode: "en",
-            TargetLanguageCode: "sv",
-            Text: "mother"
+        let data = await translate.getTerminology({
+            Name: "mcdonalds",
+            TerminologyDataFormat: "CSV"
         }).promise();
-        console.log("data")
-        console.log(data)
-        try {
-            let data = await translate.getTerminology({
-                Name: "mother",
-                TerminologyDataFormat: "CSV"
-            }).promise();
-console.log("data")
+console.log("dataa")
 console.log(data)
-
     } catch (err) {
-        console.log("errrrrrrrrrrrrr")
+        console.log("errrrr")
 console.log(err)
-            // error handling goes here
-        };
-
-    } catch (err) {
-        console.log("errrrrrrrrrrrrr")
-        console.log(err)
         // error handling goes here
     };
-    try {
-        let data = await translate.listTerminologies({
-            MaxResults: 10
-        }).promise();
-
-        console.log("data")
-        console.log(data)
-
-    } catch (err) {
-
-        console.log("errrrrrrrrrrrrr")
-        console.log(err)
-        // error handling goes here
-    };
-
-
 
     return { "message": "Successfully executed" };
 };
