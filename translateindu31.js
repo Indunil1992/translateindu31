c5fd1a04906c936e9e245bcb1c26913fd87055b6
Indunil1992 <indunil@adroitlogic.com>
2020-03-31 11:49:37 +0530

    2

--- translateindu31.js
+++ translateindu31.js
@@ -4,12 +4,25 @@ const translate = new AWS.Translate();
 exports.handler = async (event) => {
     try {
         let data = await translate.translateText({
-            SourceLanguageCode: "sv",
-            TargetLanguageCode: "en",
-            Text: "mor"
+            SourceLanguageCode: "en",
+            TargetLanguageCode: "sv",
+            Text: "mother"
         }).promise();
         console.log("data")
         console.log(data)
+        try {
+            let data = await translate.getTerminology({
+                Name: "mother",
+                TerminologyDataFormat: "CSV"
+            }).promise();
+console.log("data")
+console.log(data)
+
+    } catch (err) {
+        console.log("errrrrrrrrrrrrr")
+console.log(err)
+            // error handling goes here
+        };
 
     } catch (err) {
         console.log("errrrrrrrrrrrrr")
@@ -21,13 +34,13 @@ exports.handler = async (event) => {
             MaxResults: 10
         }).promise();
 
-console.log("data")
-console.log(data)
+        console.log("data")
+        console.log(data)
 
     } catch (err) {
 
         console.log("errrrrrrrrrrrrr")
-console.log(err)
+        console.log(err)
         // error handling goes here
     };
 
